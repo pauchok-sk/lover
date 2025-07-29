88fe39d9a512d851330ad9e0571305431175e86a
pauchok-sk <pauchoooooook@gmail.com>
2025-07-29 14:22:56 +0300

    notifications page

---
+++ dist/js/app.js
@@ -0,0 +1,61 @@
+(() => {
+    "use strict";
+    function collectStars() {
+        const countStars = document.querySelector("#count-stars");
+        const btnGet = document.querySelector("#get-stars");
+        if (countStars) {
+            btnGet.addEventListener("click", () => {
+                requestAnimationFrame(frameMain);
+                const frameFriend = () => {
+                    const num = +countStars.textContent.replace(/\s+/g, "");
+                    if (num <= 0) {
+                        countStars.textContent = 0;
+                        return;
+                    }
+                    countStars.textContent = num - (Math.round(num / 10) || 1);
+                    requestAnimationFrame(frameFriend);
+                };
+                requestAnimationFrame(frameFriend);
+            });
+            const frameMain = () => {
+                const num = +countStars.textContent.replace(/\s+/g, "");
+                if (num <= 0) {
+                    countStars.textContent = 0;
+                    return;
+                }
+                countStars.textContent = num - (Math.round(num / 10) || 1);
+                requestAnimationFrame(frameMain);
+            };
+        }
+    }
+    function copy() {
+        const buttons = document.querySelectorAll("[data-copy]");
+        if (buttons.length) buttons.forEach(btn => {
+            btn.addEventListener("click", () => {
+                const copytext = btn.dataset.copy;
+                const textBtn = btn.textContent;
+                const nextText = btn.dataset.text;
+                btn.textContent = nextText || "Copied";
+                btn.style.pointerEvents = "none";
+                setTimeout(() => {
+                    btn.textContent = textBtn;
+                    btn.style.pointerEvents = "all";
+                }, 3e3);
+                navigator.clipboard.writeText(copytext);
+            });
+        });
+    }
+    function countInput() {
+        const buttons = document.querySelectorAll("[data-count-input]");
+        if (buttons.length) buttons.forEach(btn => {
+            btn.addEventListener("click", () => {
+                const count = +btn.dataset.countInput;
+                const input = btn.closest(".input-control").querySelector(".input");
+                input.value = count;
+            });
+        });
+    }
+    collectStars();
+    copy();
+    countInput();
+})();
